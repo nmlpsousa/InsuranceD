184402d8dbe8097795a85ef04ca14efe5a8c1a2a
guirodrigues <guilhermesr@outlook.com>
2017-02-01 09:48:10 +0000

    Add getters/setters to Coverable

--- src/pt/insuranced/models/Coverable.java
+++ src/pt/insuranced/models/Coverable.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Coverable {
 	
 	private int id;
-	
+
 	private int coverableNo;
 	
 	private String description;
@@ -14,4 +14,39 @@ public class Coverable {
 	
 	private List<Coverage> coverageList;
 
+	public int getCoverableNo() {
+		return coverableNo;
+	}
+
+	public void setCoverableNo(int coverableNo) {
+		this.coverableNo = coverableNo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Claim> getClaimList() {
+		return claimList;
+	}
+
+	public void setClaimList(List<Claim> claimList) {
+		this.claimList = claimList;
+	}
+
+	public List<Coverage> getCoverageList() {
+		return coverageList;
+	}
+
+	public void setCoverageList(List<Coverage> coverageList) {
+		this.coverageList = coverageList;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
