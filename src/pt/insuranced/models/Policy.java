0898c1452cbfca091f9ac989ba2636107a4b2302
guirodrigues <guilhermesr@outlook.com>
2017-02-01 09:55:17 +0000

    Add getters/setters to Policy

--- src/pt/insuranced/models/Policy.java
+++ src/pt/insuranced/models/Policy.java
@@ -17,4 +17,52 @@ public class Policy {
 	private List<Coverable> coverableList;
 	
 	private List<Coverage> coverage;
+
+	public int getPolicyNo() {
+		return policyNo;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public Double getPremium() {
+		return premium;
+	}
+
+	public void setPremium(Double premium) {
+		this.premium = premium;
+	}
+
+	public List<Coverable> getCoverableList() {
+		return coverableList;
+	}
+
+	public void setCoverableList(List<Coverable> coverableList) {
+		this.coverableList = coverableList;
+	}
+
+	public List<Coverage> getCoverage() {
+		return coverage;
+	}
+
+	public void setCoverage(List<Coverage> coverage) {
+		this.coverage = coverage;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
