08ce9f85166ea54765f5fc27afa2f9d205bfa1cc
Nuno Sousa <nsousa@deloitte.pt>
2017-02-02 11:35:23 +0000

    Added GenericDao, CoverableDao and PaymentDao

---
+++ src/pt/insuranced/persistence/dao/CoverableDaoImpl.java
@@ -0,0 +1,24 @@
+package pt.insuranced.persistence.dao;
+
+import pt.insuranced.models.Coverable;
+import pt.insuranced.persistence.dao.sdk.interfaces.CoverableDao;
+import pt.insuranced.sdk.exceptions.InsuranceDException;
+
+import java.util.Optional;
+
+public class CoverableDaoImpl implements CoverableDao {
+    @Override
+    public Optional<Coverable> get(int id) throws InsuranceDException {
+        return null;
+    }
+
+    @Override
+    public Coverable insert(Coverable coverable) throws InsuranceDException {
+        return null;
+    }
+
+    @Override
+    public Coverable update(Coverable coverable) throws InsuranceDException {
+        return null;
+    }
+}
---
+++ src/pt/insuranced/persistence/dao/sdk/interfaces/CoverableDao.java
@@ -0,0 +1,6 @@
+package pt.insuranced.persistence.dao.sdk.interfaces;
+
+import pt.insuranced.models.Coverable;
+
+public interface CoverableDao extends GenericDao<Coverable> {
+}
