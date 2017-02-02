1d4d980f6fa8b4a47cb09b56fb11a9e50d259c57
Nuno Sousa <nsousa@deloitte.pt>
2017-02-02 16:59:10 +0000

    Changed ClaimServiceTest

--- test/pt/insuranced/services/ClaimServiceTest.java
+++ test/pt/insuranced/services/ClaimServiceTest.java
@@ -1,28 +1,26 @@
-import java.time.LocalDate;
-import java.util.Optional;
+package pt.insuranced.services;
 
 import mockit.Mock;
 import mockit.MockUp;
-import pt.insuranced.models.AbstractUser;
+import org.junit.Test;
 import pt.insuranced.models.Claim;
-import pt.insuranced.models.Client;
-import pt.insuranced.models.Password;
 import pt.insuranced.models.ReserveLine;
 import pt.insuranced.persistence.dao.ClaimDaoImpl;
 import pt.insuranced.sdk.enums.ClaimStatusEnum;
-import pt.insuranced.sdk.enums.UserStatusEnum;
-import pt.insuranced.sdk.enums.UserTypeEnum;
 import pt.insuranced.sdk.exceptions.InsuranceDException;
-import pt.insuranced.services.ClaimsService;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.fail;
 
 public class ClaimServiceTest {
 
-	@Test(expected = InsuranceDException.class)
+    @Test(expected = InsuranceDException.class)
     public void testReportClaimFail() throws InsuranceDException {
         new MockUp<ClaimDaoImpl>() {
             @Mock
-            public Claim create(Claim claim) {
-                return new InsuranceDException("An error occurred while reporting a claim.", e);
+            public Claim insert(Claim object) throws InsuranceDException {
+                throw new InsuranceDException("An error occurred while reporting a claim.");
             }
         };
         String jsonInput = "{\"id\":\"0\"}";
@@ -32,30 +30,27 @@ public class ClaimServiceTest {
         // If the method does not throw an exception, it means the test failed
         fail();
     }
-	
-	@Test
+
+    @Test
     public void testReportClaimPass() throws InsuranceDException {
-		
-		new MockUp<ClaimDaoImpl>() {
+
+        new MockUp<ClaimDaoImpl>() {
             @Mock
-            public Claim create(Claim claim) {
+            public Claim insert(Claim object) throws InsuranceDException {
                 return new Claim(1,
-                				 1,
-                				 "Claim",
-                				 LocalDate.of(2017, 1, 30),
-                				 ClaimStatusEnum.DRAFT,
-                				 new ReserveLine(1, "ReserveLine", 2000.0, 1000.0);
+                        1,
+                        "Claim",
+                        LocalDate.of(2017, 1, 30),
+                        ClaimStatusEnum.DRAFT,
+                        new ReserveLine(1, "ReserveLine", 2000.0, 1000.0));
             }
         };
-        
+
         String jsonInput = "{\"id\":\"0\"}";
         ClaimsService claimsService = new ClaimsService();
         String response = claimsService.reportClaim(jsonInput);
 
         System.out.println(response);
-        
-        // If the method does not throw an exception, it means the test failed
-        fail();
     }
-	
+
 }
