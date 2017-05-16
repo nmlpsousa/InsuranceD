bae3b40a601b4964c66afb464fb6e0415c441cbf
Nuno Sousa <nsousa@deloitte.pt>
2017-05-16 14:20:05 +0100

    Added pom configuration to assemble a single executable jar with dependencies

--- src/main/java/pt/insuranced/main/Main.java
+++ src/main/java/pt/insuranced/main/Main.java
@@ -1,11 +1,12 @@
 package pt.insuranced.main;
 
-import pt.insuranced.sdk.enums.ClaimStatusEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-    public static void main(String[] args) {
-        ClaimStatusEnum claimStatusEnum = ClaimStatusEnum.getClaimStatusByCode(2);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-        System.out.println(claimStatusEnum);
+    public static void main(String[] args) {
+        LOGGER.info("It works!");
     }
 }
