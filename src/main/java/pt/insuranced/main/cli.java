9638f93290ed83613c8580b73f333d29d098a8f0
gcoutinho <academicomr@hotmail.com>
2017-05-18 14:07:11 +0100

    Merge branch 'dev' of https://github.com/nmlpsousa/InsuranceD into dev
    # Conflicts:
    #	src/main/java/pt/insuranced/main/Main.java

--- src/main/java/pt/insuranced/main/cli.java
+++ src/main/java/pt/insuranced/main/cli.java
@@ -1,14 +1,20 @@
 package pt.insuranced.main;
 
 import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pt.insuranced.models.*;
+import pt.insuranced.persistence.dao.ClientDaoImpl;
 import pt.insuranced.sdk.enums.CountryEnum;
 import pt.insuranced.sdk.enums.UserStatusEnum;
 import pt.insuranced.sdk.enums.UserTypeEnum;
+import pt.insuranced.sdk.exceptions.InsuranceDException;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 /**
@@ -19,6 +25,14 @@ public class cli {
      * Private attribute to store parsed command-line arguments
      */
     private CommandLine cmd;
+    private Scanner scanner;
+
+    private static final Logger logger = LoggerFactory.getLogger(cli.class);
+
+    private ResourceBundle labels;
+    private Locale currentLocale;
+    private String language;
+    private String country;
 
     /**
      * Validates raw command-line arguments with respect to the set of desired options.
@@ -51,74 +65,75 @@ public class cli {
      *  Illustrates typical activity of the application with respect to values fo parsed command-line arguments
      */
     private void doSomething() {
-         /**
-         *  Option: New Client
-         */
+
+        /**
+        *  Option: New Client
+        */
         if  (cmd.hasOption("a")){
-            Scanner scanner = new Scanner(System.in);
-            scanner.useDelimiter("\\n");
             Client newClient = new Client();
             PersonalIdentification newPersonalId = new PersonalIdentification();
             Address newAddress = new Address();
             PhoneNumber newPhoneNo = new PhoneNumber();
             Password newPassword = new Password();
 
-            System.out.println("############### NEW CLIENT ###############");
+            ClientDaoImpl clientDao = new ClientDaoImpl();
+
+            System.out.println(labels.getString("newClient"));
             System.out.println(new Date());
 
             //Client Details
-            System.out.println("User Name: ");
+            System.out.println(labels.getString("userName"));
             newClient.setUsername(scanner.next());
-            System.out.println("Password: ");
+            System.out.println(labels.getString("password"));
             newPassword.setHashedPassword(scanner.next());
             newClient.setPassword(newPassword);
-            System.out.println("First Name: ");
+            System.out.println(labels.getString("firstName"));
             newPersonalId.setFirstName(scanner.next());
-            System.out.println("Last Name: ");
+            System.out.println(labels.getString("lastName"));
             newPersonalId.setLastName(scanner.next());
             //BirthDate
-            System.out.println("Birth Date (YYYY-MM-DD): ");
+            System.out.println(labels.getString("birthDate"));
             DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             dtFormatter = dtFormatter.withLocale(Locale.US); //Utilizar internaciaonalização
             LocalDate localDate = LocalDate.parse(scanner.next(), dtFormatter);
             newPersonalId.setDateOfBirth(localDate);
             //PhoneNumber
-            System.out.println("Phone Number\n");
-            System.out.println("Prefix: ");
+            System.out.println(labels.getString("phoneNo"));
+            System.out.println(labels.getString("prefix"));
             newPhoneNo.setPrefix(scanner.next());
-            System.out.println("Number: ");
+            System.out.println(labels.getString("number"));
             newPhoneNo.setNumber(scanner.nextInt());
             newPersonalId.setPhoneNumber(newPhoneNo);
             //Address
-            System.out.println("Adress ");
-            System.out.println("Enter Address Line 1: ");
+            System.out.println(labels.getString("address"));
+            System.out.println(labels.getString("addressLine1"));
             newAddress.setAddressLine1(scanner.next());
-            System.out.println("Enter Address Line 2: ");
+            System.out.println(labels.getString("addressLine2"));
             newAddress.setAddressLine2(scanner.next());
-            System.out.println("City: ");
+            System.out.println(labels.getString("city"));
             newAddress.setCity(scanner.next());
-            System.out.println("Country: ");
+            System.out.println(labels.getString("country"));
             newAddress.setCountry(CountryEnum.getCountryByCode(scanner.nextInt()));
-            System.out.println("Postal Code: ");
+            System.out.println(labels.getString("postCode"));
             newAddress.setPostalCode(scanner.next());
             newPersonalId.setAddress(newAddress);
             //Other Client Details
-            System.out.println("Email: ");
+            System.out.println(labels.getString("email"));
             newPersonalId.setEmail(scanner.next());
-            System.out.println("Identification Number: ");
+            System.out.println(labels.getString("idNo"));
             newPersonalId.setIdentificationNumber(scanner.next());
-            System.out.println("Fiscal Number: ");
+            System.out.println(labels.getString("fiscalNo"));
             newPersonalId.setFiscalNumber(scanner.next());
             newClient.setPersonalIdentification(newPersonalId);
-            System.out.println("User Status\n\n 1) Active\n 2) Inactive\n 3) Pending\n");
-            System.out.println("Select Option: ");
+            System.out.println(labels.getString("userStatusMenu"));
+            System.out.println(labels.getString("selectOption"));
             newClient.setUserStatus(UserStatusEnum.getStatusByCode(scanner.nextInt()));
-            System.out.println("User Type\n\n 1) Client\n 2) Manager\n");
-            System.out.println("Select Option: ");
+            System.out.println(labels.getString("userTypeMenu"));
+            System.out.println(labels.getString("selectOption"));
             newClient.setUserType(UserTypeEnum.getTypeByCode(scanner.nextInt()));
             System.out.println("##########################################");
 
-            System.out.println("############# CLIENT DETAILS #############");
+            System.out.println(labels.getString("clientDetails"));
             System.out.println("\nYour new client is..." +
                     "\nUsername: " +
                     newClient.getUsername() +
@@ -144,7 +159,7 @@ public class cli {
                     "\nCountry: " +
                     newClient.getPersonalIdentification().getAddress().getCountry() +
                     "\nPostal Code: " +
-                    newClient.getPersonalIdentification().getAddress().getPostalCode()+
+                    newClient.getPersonalIdentification().getAddress().getPostalCode() +
                     "\nEmail: " +
                     newClient.getPersonalIdentification().getEmail() +
                     "\nIdentification Number: " +
@@ -155,9 +170,14 @@ public class cli {
                     newClient.getUserType() +
                     "\nUser status: " +
                     newClient.getUserStatus());
-            System.out.println("############# CLIENT DETAILS #############");
-        }
+            System.out.println(labels.getString("clientDetails"));
 
+            try {
+                clientDao.insert(newClient);
+            } catch (InsuranceDException e) {
+                logger.error(labels.getString("errorInsertClient"), e);
+            }
+        }
     }
 
     /**
@@ -168,6 +188,14 @@ public class cli {
      **/
     public cli(String[] params) {
         cmd = null;
+        scanner = new Scanner(System.in);
+        scanner.useDelimiter("\\n");
+        System.out.println("Language: ");
+        language = new String(scanner.next());
+        System.out.println("Country: ");
+        country = new String(scanner.next());
+        currentLocale = new Locale(language, country);
+        labels = ResourceBundle.getBundle("LabelsBundle", currentLocale);
 
         if (validateOptions(params)) {
             doSomething();
